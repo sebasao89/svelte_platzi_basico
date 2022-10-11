abd4aef25fdc68b16ebdc072eb1404921d1e71e4
Sebas.ao <sebas.ao@outlook.com>
2022-10-10 22:26:21 -0500

    Avance hasta el video 4 (finalizado)

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,37 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  export let name;
+  export let lastName;
+  let svelteLogo = "https://arepa.s3.amazonaws.com/svelte-logo.png"
 </script>
 
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    <h1>Hello {name} - {lastName}</h1>
+    <p>Frontend Developer</p>
+    <img src={svelteLogo} alt="Logo de svelte">
   </div>
   <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
+:global(body) {
+  background-color: #f2eee2;
+  color: rgb(64, 64, 248);
+}
+
+:global(:root) {
+  --theme-color: rgb(165, 3, 165);
+  font-size: 22px;
+}
+
+p {
+  color: var(--theme-color);
+}
+
+main img {
+  width: 30%;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import './app.css'
 import App from './App.svelte'
 
 const app = new App({
-  target: document.getElementById('app')
+  target: document.getElementById('app'),
+  props: {
+    name: 'Sebastian',
+    lastName: 'Alarcon'
+  }
 })
 
 export default app
